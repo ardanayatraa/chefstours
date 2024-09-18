00395b73d824a8dc8a9cda88c9dbec700de1cd72
Ardana Yatra <imadeardanayatra@gmail.com>
2024-09-18 19:17:41 +0800

    add flowbite and update scrollable

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,15 +12,20 @@ module.exports = {
       },
       fontFamily: {
         'cs-canela': ['CS Canela', 'sans-serif'],
+        'general-sans': ['General Sans', 'sans-serif'],
       },
       screens: {
         xs: "480px",
         sm: "600px",
-        md: "768px", // Ukuran standar untuk md
+        md: "768px", 
         lg: "960px",
         xl: "1280px",
         "2xl": "1440px",
       },
+      plugins: [
+        require('flowbite/plugin')
+    ]
+
     },
   },
   plugins: [],
